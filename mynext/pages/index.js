bcbc6a38a0746dde60532a584ed8d3a35f2adecd
Johnnyo11 <118031894+Johnnyo11@users.noreply.github.com>
2023-12-08 22:08:13 +0800

    almostdabest

--- mynext/pages/index.js
+++ mynext/pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import HeroSection from"@/components/HeroSection";
+import project from "@components/project";
 
 export default function Home(){
   return(
-    
+    <div className="flex min-h-screen flex-col">
+      <HeroSection/>
+    </div>
   );
 }
