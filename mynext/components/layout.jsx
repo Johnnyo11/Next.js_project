baf136c74ed4b29d28988f7c8c68b36b1e8fe1c8
Johnnyo11 <118031894+Johnnyo11@users.noreply.github.com>
2023-12-10 22:16:38 +0800

    finally

--- mynext/components/layout.jsx
+++ mynext/components/layout.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Navibar from "./Navibar";
 
 const layout = ({ children }) => {
-    return(
-        <div className="min-h-screen mx-auto">
-            <Navibar/>
-            <main className="pl-6 pr-6 md:pl-12 md:pr-12 lg:pr-12 lg:pl-12">{children}</main>
-        </div>
-    );
+  return(
+    <div className="min-h-screen mx-auto">
+      <Navibar/>
+      <main className="pl-6 pr-6 md:pl-12 md:pr-12 lg:pr-12 lg:pl-12">{children}</main>
+    </div>
+  );
 }
 export default layout;
--- mynext/components/project.jsx
+++ mynext/components/project.jsx
@@ -8,7 +8,7 @@ const projectData = [
         description: "Proceed with caution?",
         image: "/projects/linkedout&about.png",
         youtubeUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    },
+},
 ];
 
 const Project = () => {
@@ -17,7 +17,7 @@ const Project = () => {
             <div className="text-4xl font-semibold mb-8">My Project </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-x-8 gap-y-4 h-full">
                 {projectData.map((project)=>(
-                    <Projectcard
+                    <Projectcard 
                         key={project.id}
                         title={project.title}
                         description={project.description}
