7c4a1ec99e5d6b1459aa1787f57b1e6058bbca96
Benjamin <tankerkzheng1@gmail.com>
2023-12-10 20:37:01 +0800

    fixed errors

--- mynext/components/EmailSection.jsx
+++ mynext/components/EmailSection.jsx
@@ -6,35 +6,39 @@ import Link from "next/link";
 import Image from "next/image";
 import GmailIcon from "../public/gmail-icon.svg";
 
-const EmailSection = () => { 
-
-
-    return (
-        <main className="grid md:grid-cols-2 my-12 md:my-12 py-24 md:py-24 gap-4 relative" id="contact">
-            <div className="z-10">
-                <h5 className="text-4xl font-bold text-white my-2 ">Let's Connect</h5>
-                <p className="text-[#ADB7BE] mb-4 max-w-md mt-8 text-justify">
-                I'm currently looking for new opportunities, would love to know more about new things. DO give me a heads up, I might not remember things XD
-                </p>
-                <h4 className="pb-3 mt-6">Let's get in touch! Shall we?</h4>
-                <div className=" flex flex-row gap-4 mt-6">
-                    <Link href="https://github.com/Johnnyo11">
-                        <Image src={GithubIcon} alt="Github Icon" />
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/jonathan-yeo-2905892a4/">
-                        <Image src={LinkedinIcon} alt="Linkedin Icon" />
-                    </Link>
-                    <Link href="https://www.instagram.com/johnny11_yo/" className="mt-1">
-                        <Image src={InstagramIcon}/>
-                    </Link>
-                    <Link href="mailto:jonajk111@gmail.com" className="mt-1">
-                        <Image src={GmailIcon}/>
-                    </Link>
-
-                </div>
-            </div>
-        </main>
-    );
+const EmailSection = () => {
+  return (
+    <main
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 md:py-24 gap-4 relative"
+      id="contact"
+    >
+      <div className="z-10">
+        <h5 className="text-4xl font-bold text-white my-2 ">
+          Let&apos;s Connect
+        </h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md mt-8 text-justify">
+          I&apos;m currently looking for new opportunities, would love to know
+          more about new things. DO give me a heads up, I might not remember
+          things XD
+        </p>
+        <h4 className="pb-3 mt-6">Let&apos;s get in touch! Shall we?</h4>
+        <div className=" flex flex-row gap-4 mt-6">
+          <Link href="https://github.com/Johnnyo11">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/jonathan-yeo-2905892a4/">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+          <Link href="https://www.instagram.com/johnny11_yo/" className="mt-1">
+            <Image src={InstagramIcon} />
+          </Link>
+          <Link href="mailto:jonajk111@gmail.com" className="mt-1">
+            <Image src={GmailIcon} />
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default EmailSection;
--- mynext/pages/index.js
+++ mynext/pages/index.js
@@ -1,23 +1,22 @@
 import React from "react";
-import HeroSection from"../components/HeroSection";
+import HeroSection from "../components/HeroSection";
 import Project from "../components/project";
 import Footer from "../components/Footer";
 import AboutMe from "../components/AboutMe";
-import EmailSection from '../components/EmailSection';
+import EmailSection from "../components/EmailSection";
 import AchievementsSection from "@/components/Achievement";
 
-
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <div className="flex min-h-screen flex-col">
       <main>
-        <HeroSection/>
-        <AchievementsSection/>
-        <AboutMe/>
-        <Project/>
-        <EmailSection/>
+        <HeroSection />
+        <AchievementsSection />
+        <AboutMe />
+        <Project />
+        <EmailSection />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
